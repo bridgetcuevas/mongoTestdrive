b78bb420a7321cc93ef12155472b4f210e7d4e17
bridgetcuevas <bridgetmichele@gmail.com>
2019-07-04 16:03:33 +0200

    add -should save user- mocha test

---
+++ test/create_test.js
@@ -0,0 +1,17 @@
+const assert = require( 'assert');
+const User = require('../src/user');
+
+describe('creating records', () => {
+  it('should save a user', (done) => {
+    const helen = new User({ name: 'Helen'});
+    
+    helen.save() 
+     .then(()=> {
+       //Has helen been saved successfully?
+       assert(!helen.isNew);
+       done();
+     });
+
+  });
+
+});
