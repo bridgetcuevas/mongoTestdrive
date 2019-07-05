db45d150c4b3f03ae29cc6aed1d76424394d32a4
bridgetcuevas <bridgetmichele@gmail.com>
2019-07-05 15:09:55 +0200

    add -remove/deleteOne/findOneAndRemove/findByIdAndRemove- mocha test

--- test/test_helper.js
+++ test/test_helper.js
@@ -5,19 +5,22 @@ mongoose.Promise = global.Promise;
 
 //before executed one time before all tests
 before((done) => {
-  mongoose.connect('mongodb://localhost/mongoTestdrive_test');
+  mongoose.connect(
+    'mongodb://localhost/users_test', 
+    { 
+      useNewUrlParser: true , 
+      useFindAndModify: false
+    });
   mongoose.connection
      .once('open', () => { done(); })
      .on('error', (error) => {
        console.warn('warning!', error);
      });  
 });
-
-
-
-   beforeEach((done)=>{
+  
+beforeEach((done) => {
      mongoose.connection.collections.users.drop(()=> {
        //Ready to run the next test!
        done();      
      });
-   });
+   });  
