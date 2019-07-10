2b6c3032ed7dd32fbc27f369579e3bd0848ad522
bridgetcuevas <bridgetmichele@gmail.com>
2019-07-10 11:51:29 +0200

    add posts property to user schema

--- src/user.js
+++ src/user.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const PostSchema = require('./post_schema');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema( {
@@ -10,7 +11,9 @@ const UserSchema = new Schema( {
     },
     required: [true, 'Name is required.']
   },
-  postCount: Number
+  postCount: Number,
+  //list of sub documents
+  posts: [PostSchema]
 });
 
 const User = mongoose.model('user', UserSchema);
